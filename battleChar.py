1744974a9df8532e4fee841fa64b9aa6870e8e2c
ClaushWong <36361457+ClaushWong@users.noreply.github.com>
2018-03-22 10:26:05 +0800

    Added Character for the upcoming projects
    Just a preview on the character

---
+++ battleChar.py
@@ -0,0 +1,35 @@
+char1 = {
+  'name' = 'Sophie',
+  'hp' = 500,
+  'atk' = 25,
+  'def' = 10,
+  'elmt' = 'Null'
+  'class' = 'cavalry'
+}
+
+char2 = {
+  'name' = 'Alex',
+  'hp' = 700,
+  'atk' = 45,
+  'def' = 20,
+  'elmt' = 'Null'
+  'class' = 'knight'
+}
+
+char3 = {
+  'name' = 'Lucaz',
+  'hp' = 300,
+  'atk' = 15,
+  'def' = 30,
+  'elmt' = 'Null'
+  'class' = 'shielder'
+}
+
+char4 = {
+  'name' = 'Florus',
+  'hp' = 400,
+  'atk' = 40,
+  'def' = 20,
+  'elmt' = 'Null'
+  'class' = 'mage'
+}
