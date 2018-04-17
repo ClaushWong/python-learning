b21db473d53612f286ab65e320d763cd4a41b183
ClaushWong <36361457+ClaushWong@users.noreply.github.com>
2018-04-17 10:12:50 +0800

    Add tkinder
    Learning basic of Tkinder.

---
+++ tkinder.py
@@ -0,0 +1,31 @@
+import Tkinter
+
+def println():
+    x = inputBox.get()
+    label["text"] = x
+    label.pack()
+
+def bye():
+    window.destroy()
+
+#create window
+window = Tkinter.Tk()
+window.title("MotherFucker")
+
+#create a button
+button1 = Tkinter.Button(window,text="Button1",command=println)
+button1.pack()
+
+button2 = Tkinter.Button(window,text="Button2",command=bye)
+button2.pack()
+
+#Create a line of text
+label = Tkinter.Label(window,text="Press this button to be suprised.")
+label.pack()
+
+#Create an input box
+inputBox = Tkinter.Entry(window)
+inputBox.pack()
+
+#Show the window
+window.mainloop()
